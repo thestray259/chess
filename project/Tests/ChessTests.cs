962e0dc6984aa545d99c754279cb486906ea060c
Tobie Allen <tallen@student.neumont.edu>
2023-01-06 08:51:39 -0700

    Created test project

---
+++ project/Tests/ChessTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chess; 
+
+namespace Tests
+{
+    [TestClass]
+    public class ChessTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
